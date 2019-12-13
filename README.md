795643b4365e4adce0f8ead238f933d628b007ce
divakaivan <54508530+divakaivan@users.noreply.github.com>
2019-12-13 20:28:58 +0000

    Test

---
+++ README.md
@@ -0,0 +1,4 @@
+# Comment generator
+
+When passed a word, this piece of code transforms the text into a B I G hashtag style word.
+
---
+++ src/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+with open("../README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+     name='comment_generator',
+     version='0.0.1',
+     scripts=['comment_generator'] ,
+     author="Ivan Ivanov",
+     author_email="isivanov98@outlook.com",
+     description="A Docker and AWS utility package",
+     long_description=long_description,
+     long_description_content_type="text/markdown",
+     url="https://github.com/divakaivan/icc-comments",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+ )
